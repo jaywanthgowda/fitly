f17d4faf5187ce5768c61e5e49ba2e079bdd1cc0
Jaywanth J <54073644+jaywanthgowda@users.noreply.github.com>
2025-02-07 23:59:10 +0530

    feat: database config

---
+++ backend/app/core/config.py
@@ -0,0 +1,10 @@
+from pydantic import BaseSettings
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings(BaseSettings):
+    DATABASE_URL: str = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+
+settings = Settings()
